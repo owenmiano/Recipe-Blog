1a1b931ab5a8ab8246f0d74b0aeb14b4dfa15e04
owenmiano <owenmiash90@gmail.com>
2023-10-09 10:43:56 +0300

    minor fixes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express=require('express');
 const expressLayouts=require('express-ejs-layouts');
 const fileUpload = require('express-fileupload');
-const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
+// const session = require('express-session');
+// const RedisStore = require('connect-redis')(session);
+// const redis = require('redis');
 const flash = require('connect-flash');
 const routes=require('./server/routes/recipeRoutes')
 require('dotenv').config()
 const app=express();
-const redisClient = redis.createClient({
-    host: 'localhost', // Redis server hostname
-    port: 6379,
-});
+// const redisClient = redis.createClient({
+//     host: 'localhost', // Redis server hostname
+//     port: 6379,
+// });
 const port=process.env.PORT || 3310;
 const mongoose= require('mongoose');
 const connectDB=require('./server/db');
@@ -22,12 +22,12 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.use(expressLayouts)
 
-app.use(session({
-    store: new RedisStore({ client: redisClient }),
-    secret: 'CookingBlogSecretSession',
-    resave: false,
-  saveUninitialized: false,
-}));
+// app.use(session({
+//     store: new RedisStore({ client: redisClient }),
+//     secret: 'CookingBlogSecretSession',
+//     resave: false,
+//   saveUninitialized: false,
+// }));
 
   app.use(flash());
   app.use(fileUpload());
