9ab4a344ce7c9e52e81f7fef1cc9336f8e2e9fec
owenmiano <owenmiash90@gmail.com>
2023-10-12 11:15:28 +0300

    changes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.use(express.static('/public'))
 app.use(expressLayouts)
 
 
