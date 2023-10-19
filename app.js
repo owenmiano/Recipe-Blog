4afd7fe320a05386baef35946bc3d4ac6bd00868
owenmiano <owenmiash90@gmail.com>
2023-10-19 11:01:40 +0300

    changes 103

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const flash = require('connect-flash');
 const routes=require('./server/routes/recipeRoutes')
 require('dotenv').config()
 const app=express();
-
+//load assets
+app.use('/styles', express.static(path.resolve(__dirname, "public/styles")));
+app.use('/img', express.static(path.resolve(__dirname, "public/img")));
+app.use('/js', express.static(path.resolve(__dirname, "public/js")));
+app.use('/uploads', express.static(path.resolve(__dirname, "public/uploads")));
 const port=process.env.PORT || 3310;
 const mongoose= require('mongoose');
 const connectDB=require('./server/db');
@@ -36,11 +40,7 @@ app.set('view engine','ejs')
 // Api
 app.use('/',routes)
 
-//load assets
-app.use('/styles', express.static(path.resolve(__dirname, "public/styles")));
-app.use('/img', express.static(path.resolve(__dirname, "public/img")));
-app.use('/js', express.static(path.resolve(__dirname, "public/js")));
-app.use('/uploads', express.static(path.resolve(__dirname, "public/uploads")));
+
 
 // Connect To MongoDB
 connectDB();
